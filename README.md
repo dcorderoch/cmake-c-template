cb595f174d21c6994efe15da6df851424d9385d1
David Cordero Chavarría <dcorderoch@ieee.org>
2023-05-30 22:19:59 -0600

    Add simple compile-time definition for executable

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ template for C projects, using modern CMake
 
 ## using
 
+- target-specific definition for main executable
 - target-specific include directory for the main executable
 - target-specific compile options (warnings)
 - disabling C++ via project()'s LANGUAGES C
--- src/main.c
+++ src/main.c
@@ -4,11 +4,16 @@
 
 #include <mylib.h>
 
+#if !defined(TEST_DEFINE)
+#define TEST_DEFINE 0
+#endif /* TEST_DEFINE */
+
 int
 main(void)
 {
   printf("Hello, World!\n");
   printf("mylib(): %f\n", mylib());
   printf("MODERN_MAGIC_CONSTANT: %d\n", MODERN_MAGIC_CONSTANT);
+  printf("TEST_DEFINE: %d\n", TEST_DEFINE);
   return 0;
 }
